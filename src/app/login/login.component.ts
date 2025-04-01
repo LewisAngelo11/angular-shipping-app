1a77d19163450348c758d29fd92c9c834e6d18f1
Luis Angel Soto Galvez <152150198+LewisAngelo11@users.noreply.github.com>
2025-04-01 12:44:48 -0700

    Actualización del componente del login
    Se añadieron validaciones, nuevos estilos mas modernos, y el botón de iniciar sesión con Google (No funciona aún).

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { RouterLink, RouterOutlet, Router} from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-login',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -15,24 +16,34 @@ export class LoginComponent {
   @ViewChild('password') passwordInput!: ElementRef;
 
   constructor(private authService: AuthService, private router: Router) {}
+  mensajeError: string = ''; // Nuevo campo para manejar errores
 
   login() {
     const usuario = this.usernameInput.nativeElement.value;
     const contrasena = this.passwordInput.nativeElement.value;
 
+    if (!usuario || !contrasena){ // Validar que los campos de las credenciales no estén vacíos
+      this.mensajeError = 'Por favor ingrese sus credenciales en todos los campos.'
+      return;
+    }
+
+    this.mensajeError = ''; // Borra el mensaje si todo está bien
+
     this.authService.login(usuario, contrasena).subscribe(
       (response) => {
         if (response.status === 'success') {
-          alert('Inicio de sesión exitoso: ' + response.rol);
           // Redirige al usuario a la página correspondiente
           this.router.navigate(['/menu']);
-        } else {
-          alert('Credenciales incorrectas');
         }
       },
       (error) => {
-        console.error('Error en el inicio de sesión:', error);
-        alert('Error en el servidor. Intente más tarde.');
+        console.error('Error en el inicio de sesión:', error); // Imprime toda la respuesta de error
+        // Error de autenticacion
+        if (error.status === 401) {
+          this.mensajeError = error.error.mensaje// Muestra el mensaje de error al iniciar sesión en la página
+        } else{
+          alert('Error en el servidor. Intente más tarde.');
+        }
       }
     );
   }
