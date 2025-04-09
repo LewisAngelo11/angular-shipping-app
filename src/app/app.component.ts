e8f38c3adc38c1fd78cf2d655811c941ffcd7c8f
Luis Angel Soto Galvez <152150198+LewisAngelo11@users.noreply.github.com>
2025-04-09 16:47:27 -0700

    Mejorar la interfaz del encabezado
    Se hizo mas limpia la interfaz del menú del encabezado de la página.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject} from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root', // Vincula el componente a una etiqueta HTML <app-root>
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html', // Ruta del archivo HTML asociado
   styleUrls: ['./app.component.css'] // Ruta(s) del archivo CSS asociado
 })
 export class AppComponent {
+  constructor(private router: Router, private authService: AuthService){}
 
+  // Método para verificar si el usuario está autenticado (si el token existe)
+  isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
+  
+  irLogin(){
+    this.router.navigate(['/login']); // Navega a la ruta '/login'
+  }
+
+  miCuenta(){
+    this.router.navigate(['/miCuenta']);
+  }
 }
 
