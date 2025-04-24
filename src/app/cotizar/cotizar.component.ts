16ebd6d96831103fc392ba1df0fb62b15684936a
Luis Angel Soto Galvez <152150198+LewisAngelo11@users.noreply.github.com>
2025-04-23 23:23:11 -0700

    Mejora de interfaz gráfica del componente cotizar
    Solo son avances, no definitivo aún estos avances.

--- src/app/cotizar/cotizar.component.ts
+++ src/app/cotizar/cotizar.component.ts
@@ -2,10 +2,11 @@ import { Component,ViewChild,ElementRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';  // Aquí se importa FormsModule
 
 @Component({
   selector: 'app-cotizar',
-  imports: [RouterOutlet ,CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './cotizar.component.html',
   styleUrl: './cotizar.component.css'
 })
@@ -14,40 +15,65 @@ export class CotizarComponent {
   title = "Origen"
   title2 = "Destino"
   title3 = "Paquete"
-  title4 = "Remitente"
-  title5 = "Destinatario"
+  title4 = "¿Quién Envía?"
+  title5 = "¿Quién Recibe?"
   txtPeso = "Mi peso"
   txtPesoVM = ""
   txtTarifa = ""
   txtIVA = ""
   txtCosto = ""
   txtDistancia = ""
+  // Propiedades para los datos del usuario
+  nombre: string = '';
+  apellidoPaterno: string = '';
+  apellidoMaterno: string = '';
+  email: string = '';
+  // Inputs de Origen
   @ViewChild('CP') CP!: ElementRef;
   @ViewChild('Municipio') Mun!: ElementRef;
   @ViewChild('Ciudad') CD!: ElementRef;
   @ViewChild('Estado') Entidad!: ElementRef;
+  // Inputs de Destino
   @ViewChild('CPD') CPD!: ElementRef;
   @ViewChild('MunicipioD') MunD!: ElementRef;
   @ViewChild('EstadoD') EntidadD!: ElementRef;
   @ViewChild('CiudadD') CDD!: ElementRef;
+  // Inputs del paquete
   @ViewChild('Peso') Peso!: ElementRef;
   @ViewChild('Largo') Largo!: ElementRef;
   @ViewChild('Alto')Alto!: ElementRef;
   @ViewChild('Ancho') Ancho!: ElementRef;
+  // Inputs del Remitente
   @ViewChild('nombreR') NombreR!: ElementRef;
   @ViewChild('ape1R') Apellido1R!: ElementRef;
   @ViewChild('ape2R') Apellido2R!: ElementRef;
   @ViewChild('emailR') EmailR!: ElementRef;
+  // Inputs del Destinatario
   @ViewChild('nombreD') NombreD!: ElementRef;
   @ViewChild('ape1D') Apellido1D!: ElementRef;
   @ViewChild('ape2D') Apellido2D!: ElementRef;
   @ViewChild('emailD') EmailD!: ElementRef;
-  @ViewChild('Tarifa') Tarifa!: ElementRef;
   @ViewChild('fechaR') FechaR!: ElementRef;
+  @ViewChild('Tarifa') Tarifa!: ElementRef;
   constructor(private authService: AuthService) {}
 
-  buscarCPorigen(){
+  ngOnInit() {
+    if (this.authService.isAuthenticated()){
+      this.authService.ConsultarUser().subscribe(
+        perfil => {
+          this.nombre = perfil.Nombre;
+          this.apellidoPaterno = perfil.Apellido1;
+          this.apellidoMaterno = perfil.Apellido2;
+          this.email = perfil.Email;
+        },
+        error => {
+          console.error("Error al obtener el perfil:",error);
+        }
+      );  
+    }
+  }
 
+  buscarCPorigen(){
     const body = {
       CP: this.CP.nativeElement.value,
     };
@@ -55,7 +81,6 @@ export class CotizarComponent {
     this.authService.EnocntrarCP(body).subscribe(
       (response) => {
         if (response.status === 'success'){
-          //alert('datos obtenidos: ' + response.rol);
           this.Mun.nativeElement.value = response.municipio
           this.CD.nativeElement.value = response.localidad
           this.Entidad.nativeElement.value = response.entidad
@@ -71,7 +96,6 @@ export class CotizarComponent {
   }
 
   buscarCPdestino(){
-
     const body = {
       CP: this.CPD.nativeElement.value,
     };
@@ -134,14 +158,13 @@ export class CotizarComponent {
       }
     );
   }
+
   mostrarContenido() { 
     this.contenidoVisible = !this.contenidoVisible; 
     this.txtPeso = this.Peso.nativeElement.value;
   }
 
-
   EnviarPaquete(){
-
     const body = {
       tarifa: this.txtCosto,
       Origen: this.Entidad.nativeElement.value + "," + this.CD.nativeElement.value,
@@ -159,15 +182,14 @@ export class CotizarComponent {
       Apellido2D: this.Apellido2D.nativeElement.value,
       EmailD: this.EmailD.nativeElement.value,
       FechaR: this.FechaR.nativeElement.value
-
     }
 
     this.authService.enviarpaquete(body).subscribe(
       (response) => {
         if (response.status === 'success'){
-          alert('entrega añadida Codigo de rastreo: ' + response.Rastreo_Code);
+          alert('Entrega añadida Codigo de rastreo: ' + response.Rastreo_Code);
         } else {
-          alert('Error al registrar el usuario');
+          alert('Error al registrar la entrega');
         }
       },
       (error) =>{
